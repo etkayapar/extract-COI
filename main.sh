691ed1508bfdef3ada8961ebfda46db0508469f8
Etka Yapar <etka.yapar@gmail.com>
2024-03-27 14:47:55 +0100

    Move scripts to scripts/, add add the absolute path to script calls

--- main.sh
+++ main.sh
@@ -8,13 +8,15 @@
 genome_paths=$1 ## path to a text file with full paths to assemblies
 threads=$2 ## number of independent parallel processes to run
 
+scr_root=$(dirname "$0")
+
 ## Create blast databases ------------------------------------------
 mkdir -p databases
 if [[ ! -n $(ls -A databases/) ]]
 then
 	rm -rf blast_results/ all_COI.fa*
 	echo -e "Creating blast databases for the input assemblies..."
-	cat ${genome_paths} | xargs -n 1 -P $threads ./makeblastdb_single.sh
+	cat ${genome_paths} | xargs -n 1 -P $threads ${scr_root}/makeblastdb_single.sh
 	echo -e "Done.\n"
 else
 	echo -e "Skipping blast db creation since './databases/' is not empty\n"
@@ -26,7 +28,7 @@ if [[ ! -n $(ls -A blast_results | grep "blastn.tsv") ]]
 then
 	rm -rf blast_results/*.fasta all_COI.fa*
 	echo -e "Running blast..."
-	cat ${genome_paths} | xargs -n1 -P $threads ./run_blast_single.sh
+	cat ${genome_paths} | xargs -n1 -P $threads ${scr_root}/run_blast_single.sh
 	echo -e "Done.\n"
 else
 	echo -e "Skipping running blast since './blast_results/' has results from previous runs\n"
@@ -40,7 +42,7 @@ then
 	echo -e "Extracting COI sequences according to blast results..."
 	cp ${genome_paths} blast_results/genomes.txt
 	cd blast_results
-	../extract_seq_from_blast.py
+	${scr_root}/extract_seq_from_blast.py
 	cd ..
 	echo -e "Done.\n"
 else
