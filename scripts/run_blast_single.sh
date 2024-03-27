1ee63e60a931c3af837c91342a782d7160945770
Etka Yapar <etka.yapar@gmail.com>
2024-03-27 14:54:29 +0100

    Rename COI_bombyx.fasta and all occurences

--- scripts/run_blast_single.sh
+++ scripts/run_blast_single.sh
@@ -4,4 +4,4 @@ sp=$1
 sp=${sp##*/}
 sp=${sp%%.*}
 
-tblastn -db databases/${sp} -query COI_bombyx.fasta -out blast_results/${sp}_tblastn.tsv -task tblastn -outfmt "7 sseqid qseqid sstart send sframe evalue bitscore qlen length"
+tblastn -db databases/${sp} -query COI_query.fasta -out blast_results/${sp}_tblastn.tsv -task tblastn -outfmt "7 sseqid qseqid sstart send sframe evalue bitscore qlen length"
